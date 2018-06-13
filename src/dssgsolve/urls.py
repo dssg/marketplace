f690709df9fa71dad6128645779538fe67efad2f
Alberto Bengoa <me@albertobengoa.com>
2018-06-13 15:20:48 -0500

    Moved the urls for DSSG Solve to the root, instead of having them under /dssg/

--- src/dssgsolve/urls.py
+++ src/dssgsolve/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('dssg/', include('dssgmkt.urls')),
+    path('', include('dssgmkt.urls')),
     path('admin/', admin.site.urls),
 ]
