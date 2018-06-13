29cb872800d77db53d0cb3d03fdf9d50b16e36c5
Alberto Bengoa <me@albertobengoa.com>
2018-06-13 15:16:46 -0500

    Added a default value for the DEBUG property. It is now set to False by default.

--- src/dssgsolve/settings.py
+++ src/dssgsolve/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config(('DEBUG'), cast=bool)
+DEBUG = config('DEBUG', default = False, cast = bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast = Csv())
 
 
 # Application definition
