42eec65c0afa8864bb1c2f4ae825e617e3954773
Alberto Bengoa <me@albertobengoa.com>
2018-06-14 14:25:14 -0500

    Removed unused import AbstractUser from the organization models

--- src/dssgmkt/models/org.py
+++ src/dssgmkt/models/org.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django_countries.fields import CountryField
-from django.contrib.auth.models import AbstractUser
 from dssgsolve import settings
 
 from .common import (PHONE_REGEX, MAIN_CAUSE_CHOICES, CAUSE_EDUCATION,
