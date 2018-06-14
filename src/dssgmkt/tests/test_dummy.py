0c289758a4a400ec3f2e072bb0a891dbc8c9471f
Alberto Bengoa <me@albertobengoa.com>
2018-06-14 12:57:45 -0500

    Moved tests to a subfolder

---
+++ src/dssgmkt/tests/test_dummy.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+class DummyTestCase(TestCase):
+    def setUp(self):
+        super.setUp()
+
+    def test_false_assertion(self):
+        # This should never pass
+        self.assertEqual(0, 1)
