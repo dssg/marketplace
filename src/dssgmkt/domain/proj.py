6a06a4780e43d0052a965f4e8730fd3f43c999fa
Alberto Bengoa <me@albertobengoa.com>
2018-07-12 14:37:51 -0500

    Do not show a button for applying to a task if the user has pending applications to that task.

--- src/dssgmkt/domain/proj.py
+++ src/dssgmkt/domain/proj.py
@@ -13,7 +13,7 @@ from ..models.common import (
 from ..models.user import (
     User, NotificationSeverity, NotificationSource, VolunteerProfile,
 )
-from django.db.models import Case, When, Count, Q
+from django.db.models import Case, When, Count, Q, Subquery
 
 from .common import validate_consistent_keys
 from .notifications import NotificationService
@@ -403,7 +403,11 @@ class ProjectTaskService():
 
     @staticmethod
     def get_public_tasks(request_user, proj):
-        return ProjectTask.objects.filter(project=proj).exclude(stage=TaskStatus.DELETED).annotate(volunteer_count=Count('projecttaskrole', filter=Q(projecttaskrole__role=TaskRole.VOLUNTEER))).order_by('-accepting_volunteers', '-stage')
+        return ProjectTask.objects.filter(project=proj) \
+                                    .exclude(stage=TaskStatus.DELETED) \
+                                    .annotate(volunteer_count=Count('projecttaskrole', filter=Q(projecttaskrole__role=TaskRole.VOLUNTEER))) \
+                                    .annotate(already_applied=Count('volunteerapplication', filter=Q(volunteerapplication__volunteer=request_user, volunteerapplication__status=ReviewStatus.NEW))) \
+                                    .order_by('-accepting_volunteers', '-stage')
 
     @staticmethod
     def get_non_finished_tasks(request_user, proj):
