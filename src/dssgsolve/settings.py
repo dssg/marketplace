3b0464a88580ad3b0715bf72f20d5226a3ff692a
Alberto Bengoa <me@albertobengoa.com>
2018-07-17 16:24:02 -0500

    Added Whitenoise to handle static files in non-debug mode. Added a mock 404 page.

--- src/dssgsolve/settings.py
+++ src/dssgsolve/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'dssgmkt/static')
 
 
 
