3322e77b5d7d4433a8c5471afb8e9dcea8830a77
Jesse London <jesselondon@gmail.com>
2018-08-01 17:10:44 -0500

    adjustments to storage of static files in AWS S3
    we store static files with public access, so it's unnecessary to add credentials to their URLs (and might add overhead and interfere with caching)
    
    upload static files under the "static/" prefix (in case we need this bucket for *anything* else)

--- src/dssgsolve/settings.py
+++ src/dssgsolve/settings.py
@@ -180,6 +180,9 @@ if file_storage_option == 's3':
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+    AWS_QUERYSTRING_AUTH = False
+    AWS_LOCATION = 'static'
+
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=None)
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default=None)
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
