13894db22708f00af60de6615697102ce2fdb7c4
Jesse London <jesselondon@gmail.com>
2018-08-02 12:55:30 -0500

    re-enabled SMTP email configuration for use in production

--- src/dssgsolve/settings.py
+++ src/dssgsolve/settings.py
@@ -212,11 +212,11 @@ if DEBUG:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-    # EMAIL_HOST = config('EMAIL_HOST')
-    # EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-    # EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-    # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-    # EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 if DEBUG:
