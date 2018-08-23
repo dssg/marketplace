661caf9d7c8215c2656d39b090d853bdeca96f75
Jesse London <jesselondon@gmail.com>
2018-08-23 18:45:42 -0500

    fully automated deployment (#9)
    ## Branch summary
    
    This branch implements the build deploy sub-command – `manage build deploy` – and includes some minor clean-up of the build command.
    
    This branch also adds a management-specific, environment-based configuration; (though I certainly could have, I wasn't sure it would make sense to mix this with the app-specific configuration file). Because the management commands only use the environmental values as defaults, and otherwise provide an obvious interface for supplying these (the CLI), this is only environmental – (it does not use a `decouple` _.env_ file) – but, having optionally installed `direnv`, and populated a _.envrc_ file from the example, you similarly get file-based configuration, anyway.
    
    ## Set-up
    
    This probably belongs elsewhere, but I'm not really sure right now. Moreover, the folks who need this probably have already received it several times in other places (Slack). But again to be sure:
    
    * You'll need AWS credentials, in our case for the dsapp-ddj account. _.envrc.example_ suggests you set `AWS_PROFILE`, but of course you can simply set `AWS_ACCESS_KEY_ID` and the secret.
    * You'll need unencrypted access to the marketplace server PEM. I believe everyone is now set-up with Password Store.
    * You should only really need to have the CLI requirements installed, *i.e.*: `pip install -r requirement/console.txt`. But, I believe everyone has used the `develop` script, which should do the trick.
    
    NBD, right?
    
    ## Command summary
    
    Remember to tag first (and push that tag). I haven't gotten us past `0.0.0` yet. The last was `0.0.0b10` :stuck_out_tongue: 
    
    Check out `manage -h`, `manage build -h` and `manage build deploy -h`.
    
    For a super-simple build deployment, *i.e.* one requiring neither schema migrations nor refreshment of static assets in S3, you can do it all in one shot, (assuming anyway that your defaults are completely set up via the environment):
    
        manage build --label=THE_NEW_TAG --login --push --deploy
    
    or tersely:
    
        manage build --label=THE_NEW_TAG -lpd
    
    (though `--login` is only necessary if you haven't done so recently).
    
    If you need to pass interesting options to the deploy command -- for example, if you want to have it automatically migrate the database or refresh S3, then don't `--deploy`; rather, having run `build`, `deploy` separately, *e.g.*:
    
        manage build deploy [--static] [--migrate]
    
    ## Changes
    
    * added .envrc structure for CLI environment variables and implemented build deploy (service restart)
    
    * fixed invocation of deployment subcommand in combination with build
    
    * allowable line-length configuration for flake8
    
    * factored out handling for CLI options backed by environment variables into argparse action
    
    and fixed an option's help text
    
    * use argcmdr development feature `delegate` to simplify sub-command delegation
    
    ditto: `print_command`
    
    (note: will require argcmdr version increment)
    
    * post-deployment task handling
    
    * removed stale comment
    
    * fixed ssh commands used for post-deployment tasks
    
    * debugging
    
    * added documentation of deployment to README
    
    * README refinement
    
    * bootstrapping refinements
    
      * when a user installed pyenv for the first time, it wasn't configured
    for immediate use
        * a user may never set up pyenv correctly, but we needn't continually
      install it; (in which case, we can let things fail)
      * a user may not have the directory ~/.local/bin
        * ~/.local/bin may not be on the user's PATH, (though ECS-CLI may in
      fact have been installed)
      * as far as I can tell AWS-CLI was never accounted for; (but, in fact,
    it's required, even though so far ECS-CLI is not)

--- manage.py
+++ manage.py
@@ -1,19 +1,128 @@
+import datetime
+import enum
+import itertools
+import json
 import os
-from argparse import REMAINDER
+import re
+import sys
+import time
+from argparse import _StoreAction, REMAINDER
 from pathlib import Path
 
 from argcmdr import Local, LocalRoot, localmethod
+from terminaltables import AsciiTable
 
 
 ROOT_PATH = Path(__file__).parent.resolve()
 SRC_PATH = ROOT_PATH / 'src'
 
-IMAGE_REPOSITORY_NAME_DEFAULT = os.getenv('IMAGE_REPOSITORY_NAME')
-IMAGE_REPOSITORY_URI_DEFAULT = os.getenv('IMAGE_REPOSITORY_URI')
+
+def spincycle(chars='|/–\\', file=sys.stdout, wait=1):
+    """A generator which writes a "spinner" to file, cycling through
+    the given characters, with the given wait between writes.
+
+    The last written character is yielded to the iterator, such that a
+    polling procedure may be executed with the given wait, while the
+    file (stdout) is updated.
+
+    """
+    for char in itertools.cycle(chars):
+        file.write(char)
+        file.flush()
+
+        yield char
+
+        time.sleep(wait)
+        file.write('\b')
 
 
-def add_default(text, default):
-    return f'{text} (default: {default})' if default else text
+def store_env_override(option_strings,
+                       dest,
+                       envvar,
+                       nargs=None,
+                       default=None,
+                       type=None,
+                       choices=None,
+                       description=None,
+                       help=None,
+                       metavar=None):
+    """Construct an argparse action which stores the value of a command
+    line option to override a corresponding value in the process
+    environment.
+
+    If the environment variable is not empty, then no override is
+    required. If the environment variable is empty, and no default is
+    provided, then the "option" is required.
+
+    In the case of a default value which is a *transformation* of the
+    single environment variable, this default may be provided as a
+    callable, (*e.g.* as a lambda function).
+
+    Rather than have to fully explain the relationship of this
+    environment-backed option, help text may be generated from a
+    provided description.
+
+    """
+    if envvar == '':
+        raise ValueError("unsupported environment variable name", envvar)
+
+    envvalue = os.getenv(envvar)
+
+    if callable(default):
+        default_value = default(envvalue)
+    elif envvalue:
+        default_value = envvalue
+    else:
+        default_value = default
+
+    if description and help:
+        raise ValueError(
+            "only specify help to override its optional generation from "
+            "description -- not both"
+        )
+    elif description:
+        if default_value:
+            help = '{} (default {} envvar {}: {})'.format(
+                description,
+                'provided by' if default is None else 'derived from',
+                envvar,
+                default_value,
+            )
+        else:
+            help = (f'{description} (required because '
+                    f'envvar {envvar} is empty)')
+
+    return _StoreAction(
+        option_strings=option_strings,
+        dest=dest,
+        nargs=nargs,
+        const=None,
+        default=default_value,
+        type=type,
+        choices=choices,
+        required=(not default_value),
+        help=help,
+        metavar=metavar,
+    )
+
+
+class ContainerRegistryMixin:
+
+    def __init__(self, parser):
+        super().__init__(parser)
+
+        parser.add_argument(
+            '--repository-uri',
+            action=store_env_override,
+            envvar='IMAGE_REPOSITORY_URI',
+            description='image repository URI',
+        )
+        parser.add_argument(
+            '--repository-name',
+            action=store_env_override,
+            envvar='IMAGE_REPOSITORY_NAME',
+            description='image repository name',
+        )
 
 
 class Marketplace(LocalRoot):
@@ -21,40 +130,30 @@ class Marketplace(LocalRoot):
 
 
 @Marketplace.register
-class Build(Local):
+class Build(ContainerRegistryMixin, Local):
     """build app container image"""
 
     def __init__(self, parser):
-        default_name = IMAGE_REPOSITORY_NAME_DEFAULT and (IMAGE_REPOSITORY_NAME_DEFAULT + ':latest')
+        super().__init__(parser)
 
         parser.add_argument(
-            '--repository-uri',
-            default=IMAGE_REPOSITORY_URI_DEFAULT,
-            help=add_default('Image repository URI',
-                             IMAGE_REPOSITORY_URI_DEFAULT),
-        )
-        parser.add_argument(
-            '--repository-name',
-            default=IMAGE_REPOSITORY_NAME_DEFAULT,
-            help=add_default('Image repository name',
-                             IMAGE_REPOSITORY_NAME_DEFAULT),
-        )
-        parser.add_argument(
             '-n', '--name',
-            default=default_name,
-            help=add_default('Image name:tag', default_name),
+            action=store_env_override,
+            envvar='IMAGE_REPOSITORY_NAME',
+            default=lambda envvalue: envvalue and f'{envvalue}:latest',
+            description='image name:tag',
         )
         parser.add_argument(
             '--label',
             action='append',
-            help='Additional name/tags to label image; the first of these, '
+            help='additional name/tags to label image; the first of these, '
                  'if any, is treated as the "version"',
         )
         parser.add_argument(
             '--target',
             choices=('development', 'production'),
             default='production',
-            help="Target environment (default: production)",
+            help="target environment (default: production)",
         )
         parser.add_argument(
             '-l', '--login',
@@ -73,28 +172,9 @@ class Build(Local):
         )
 
     def get_full_name(self, name):
-        if not self.args.repository_uri:
-            self.args.__parser__.error(
-                "image repository URI required "
-                "(specify argument --repository-uri or "
-                "environment variable IMAGE_REPOSITORY_URI)"
-            )
-
         return '/'.join((self.args.repository_uri, name))
 
     def prepare(self, args, parser):
-        dynamically_missing = [
-            argument_descriptor for (argument_descriptor, argument_value) in (
-                ('-n/--name', args.name),
-                ('--repository-name', args.repository_name),
-            ) if not argument_value
-        ]
-        if dynamically_missing:
-            parser.error(
-                "the following argument values could not be dynamically determined: " +
-                ', '.join(dynamically_missing)
-            )
-
         if args.login and not args.push:
             parser.error("will not log in outside of push operation")
 
@@ -116,14 +196,14 @@ class Build(Local):
         yield command[ROOT_PATH]
 
         if args.push:
-            yield from self['push'].prepare(args)
+            yield from self['push'].delegate()
 
         if args.deploy:
-            yield self['deploy'].prepare(args)
+            yield from self['deploy'].delegate()
 
     @localmethod('-l', '--login', action='store_true', help="log in to AWS ECR")
     def push(self, args):
-        """push already-built image to registry"""
+        """push latest image to registry"""
         if args.login:
             login_command = self.local['aws'][
                 'ecr',
@@ -131,8 +211,7 @@ class Build(Local):
                 '--no-include-email',
                 '--region', 'us-west-2',
             ]
-            if args.show_commands or not args.execute_commands:
-                print('>', login_command)
+            self.print_command(login_command)
             if args.execute_commands:
                 full_command = login_command()
                 (executable, *arguments) = full_command.split()
@@ -144,23 +223,229 @@ class Build(Local):
             self.get_full_name(args.name),
         ]
 
-    @localmethod
-    def deploy(self, args):
-        """deploy an image container"""
-        # FIXME
-        raise NotImplementedError
+    class Deploy(Local):
+        """deploy the latest image container to the cluster service"""
 
-        command = self.local['eb']['deploy']
+        TASK_START_PATTERN = re.compile(r'\(service (?P<service_name>[\w-]+)\) '
+                                        r'has started (?P<number_tasks>\d+) '
+                                        r'tasks: \(task (?P<task_id>[\w-]+)\)\.')
 
-        # specify environment
-        if args.target == 'production':
-            command = command['appy-reviews-pro']
-        else:
-            command = command['appy-reviews-dev']
+        class UpdateServiceColumns(str, enum.Enum):
 
-        if args.label:
-            return command['-l', args.label[0]]
-        return command
+            id = 'ID'
+            status = 'Status'
+            desiredCount = 'Desired'
+            pendingCount = 'Pending'
+            createdAt = 'Created'
+            updatedAt = 'Updated'
+
+            def __str__(self):
+                return self.value.__str__()
+
+            def get_string(self, data):
+                value = data[self.name]
+
+                if self is self.createdAt or self is self.updatedAt:
+                    return datetime.datetime.fromtimestamp(value).strftime('%Y-%m-%d %H:%M:%S')
+
+                return str(value)
+
+        def __init__(self, parser):
+            parser.add_argument(
+                '--cluster',
+                action=store_env_override,
+                envvar='ECS_CLUSTER_NAME',
+                description="short name or full Amazon Resource Name (ARN) "
+                            "of the cluster that your service is running on",
+            )
+            parser.add_argument(
+                '--service',
+                action=store_env_override,
+                envvar='ECS_SERVICE_NAME',
+                description="name of the service to update",
+            )
+
+            parser.add_argument(
+                '--static',
+                action='store_true',
+                help="collect and deploy static assets upon deployment",
+            )
+            parser.add_argument(
+                '--migrate',
+                action='store_true',
+                help="migrate the database upon deployment",
+            )
+
+            parser.add_argument(
+                '--ssh',
+                action=store_env_override,
+                default='ssh',
+                envvar='ECS_SSH',
+                metavar='COMMAND',
+                description='ssh command through which to execute post-deployment tasks',
+            )
+
+            # Note: This override only works when command is called
+            # outright, not when delegated:
+            parser.add_argument(
+                '--no-quiet',
+                action='store_true',
+                default=False,
+                dest='foreground',
+                help="print command output",
+            )
+            parser.add_argument(
+                '-qq',
+                action='store_false',
+                default=True,
+                dest='report',
+                help="do not print deployment result",
+            )
+
+        def prepare(self, args, parser, local):
+            last_event = None
+            if args.static or args.migrate:
+                # AWS CLI and ECS CLI provide no great way of tying deployments
+                # to containers and instances, which makes this quite annoying.
+                #
+                # Retrieve a baseline event:
+                (_retcode, stdout, _stderr) = yield local['aws'][
+                    'ecs',
+                    'describe-services',
+                    '--cluster', args.cluster,
+                    '--services', args.service,
+                ]
+                if stdout is not None:
+                    result = json.loads(stdout)
+                    (service,) = result['services']
+                    last_event = service['events'][0]
+
+            # update service (deploy)
+            (_retcode, stdout, _stderr) = yield local['aws'][
+                'ecs',
+                'update-service',
+                '--force-new-deployment',
+                '--cluster', args.cluster,
+                '--service', args.service,
+            ]
+
+            # report on deployments
+            if args.report and stdout is not None:
+                try:
+                    result = json.loads(stdout)
+
+                    service_name = result['service']['serviceName']
+                    deployments = result['service']['deployments']
+
+                    data = [self.UpdateServiceColumns]
+                    data.extend(
+                        [
+                            column.get_string(deployment)
+                            for column in self.UpdateServiceColumns
+                        ]
+                        for deployment in deployments
+                    )
+                except (KeyError, ValueError):
+                    print('unexpected command output:', stdout, file=sys.stderr, sep='\n')
+                else:
+                    table = AsciiTable(data, title=f"{service_name} deployments")
+                    print(table.table)
+
+            # post-deployment actions
+            if last_event:
+                # there are post-deployment actions and this is not a dry run
+                #
+                # poll event stream to discover newly-started task reflecting
+                # deployed version
+                spinner = spincycle(['']) if args.show_commands else spincycle()
+                for _cycle in spinner:
+                    (_retcode, stdout, _stderr) = yield local['aws'][
+                        'ecs',
+                        'describe-services',
+                        '--cluster', args.cluster,
+                        '--services', args.service,
+                    ]
+                    result = json.loads(stdout)
+                    (service,) = result['services']
+                    events = tuple(itertools.takewhile(
+                        lambda event: event['id'] != last_event['id'],
+                        service['events']
+                    ))
+                    for event in reversed(events):
+                        task_start_match = self.TASK_START_PATTERN.match(event['message'])
+                        if task_start_match:
+                            task_id = task_start_match.group('task_id')
+                            break
+                    else:
+                        # no match -- skip cycle
+                        continue
+
+                    # match found -- break loop
+                    print('\b')
+                    break
+
+                # trace task to its EC2 instance
+                # (AWS CLI you are terrible)
+                (_retcode, stdout, _stderr) = yield local['aws'][
+                    'ecs',
+                    'describe-tasks',
+                    '--cluster', args.cluster,
+                    '--tasks', task_id,
+                ]
+                result = json.loads(stdout)
+                (task,) = result['tasks']
+
+                (_retcode, stdout, _stderr) = yield local['aws'][
+                    'ecs',
+                    'describe-container-instances',
+                    '--cluster', args.cluster,
+                    '--container-instances', task['containerInstanceArn'],
+                ]
+                result = json.loads(stdout)
+                (container_instance,) = result['containerInstances']
+
+                (_retcode, stdout, _stderr) = yield local['aws'][
+                    'ec2',
+                    'describe-instances',
+                    '--instance-ids', container_instance['ec2InstanceId'],
+                ]
+                result = json.loads(stdout)
+                (ec2_reservation,) = result['Reservations']
+                (ec2_instance,) = ec2_reservation['Instances']
+                public_ip = ec2_instance['PublicIpAddress']
+
+                (ssh_exec, *ssh_args) = args.ssh.split()
+                ssh = local[ssh_exec].bound_command(*ssh_args)[public_ip]
+
+                image_path = '/'.join((args.repository_uri, args.repository_name))
+                for _cycle in spinner:
+                    # FIXME: We've seen it not here tho it should be: investigate
+                    (_retcode, stdout, _stderr) = yield ssh['docker'][
+                        'ps',
+                        '--filter', f'ancestor={image_path}',
+                        '--format', '"{{.Names}}"',
+                    ]
+                    try:
+                        (container_name,) = stdout.splitlines()
+                    except ValueError:
+                        continue
+                    else:
+                        print('\b')
+                        break
+
+                container = ssh['docker']['exec', '-u', 'webapp', container_name]
+
+                if args.migrate:
+                    yield (
+                        local.FG(retcode=None),
+                        container['./manage.py', 'migrate']
+                    )
+
+                if args.static:
+                    yield (
+                        local.FG(retcode=None),
+                        container['./manage.py', 'collectstatic', '--no-input', '-v', '1']
+                    )
 
 
 @Marketplace.register
